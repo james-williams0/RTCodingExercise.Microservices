0446e8d3eabba601cc2d9b70bf032e5a8b398fa4
jawil <9088026+james-williams0@users.noreply.github.com>
2025-05-27 00:29:42 +0100

    Make catalog API swagger work

--- src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ public class HomeController : Controller
         _context = context;
     }
     
+    [HttpGet]
     public IActionResult Index()
     {
         return new RedirectResult("~/swagger");
