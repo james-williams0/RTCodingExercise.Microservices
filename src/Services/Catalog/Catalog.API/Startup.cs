fd3b8b0e192bb3286e39313f05cb5f4262d8c6eb
jawil <9088026+james-williams0@users.noreply.github.com>
2025-05-27 00:30:37 +0100

    Add IApplicationDbContext to catalog API DI

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -17,7 +17,7 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         // Add framework services.
-        services.AddDbContext<ApplicationDbContext>(options =>
+        services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration["ConnectionString"],
                 sqlServerOptionsAction: sqlOptions =>
                 {
