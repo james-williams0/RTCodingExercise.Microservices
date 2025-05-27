336984c51a4bb8ee763753ff279bc7be44c3352c
jawil <9088026+james-williams0@users.noreply.github.com>
2025-05-27 20:43:51 +0100

    Fuzzy searcher for similar registrations

---
+++ src/Services/Catalog/Catalog.API/Services/PlateFuzzySearcher.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Catalog.API.Services;
+
+public interface IPlateFuzzySearcher
+{
+    bool IsMatch(string registration, string searchTerm);
+}
+
+public class PlateFuzzySearcher : IPlateFuzzySearcher
+{
+    private readonly Dictionary<char, char[]> _characterSubstitutions = new()
+    {
+        {'A', ['4', 'A'] },
+        {'B', ['8', 'B'] },
+        {'E', ['3', 'E'] },
+        {'G', ['6', 'G'] },
+        {'I', ['1', 'I'] },
+        {'O', ['0', 'O'] },
+        {'S', ['5', 'S', '2'] },
+        {'T', ['7', 'T'] },
+        {'Z', ['2', 'Z'] },
+    };
+
+    public bool IsMatch(string registration, string searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            return false;
+        }
+
+        var searchableReg = registration.Replace(" ", "").ToUpperInvariant();
+        searchTerm = searchTerm.Replace(" ", "").ToUpperInvariant();
+        
+        var patternBuilder = new StringBuilder();
+        patternBuilder.Append(".*");
+    
+        foreach (var character in searchTerm)
+        {
+            if (_characterSubstitutions.TryGetValue(character, out var substitutes))
+            {
+                patternBuilder.Append('[');
+                foreach (var substitute in substitutes)
+                {
+                    patternBuilder.Append(substitute);
+                }
+                patternBuilder.Append(']');
+            }
+            else
+            {
+                patternBuilder.Append(character);
+            }
+        
+            patternBuilder.Append(".*");
+        }
+    
+        return Regex.IsMatch(searchableReg, patternBuilder.ToString());
+    }
+}
---
+++ src/Services/Catalog/Catalog.UnitTests/Services/PlateFuzzySearcherTests.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.API.Services;
+using Xunit;
+
+namespace Catalog.UnitTests.Services;
+
+public class PlateFuzzySearcherTests
+{
+    [Theory]
+    [InlineData("Danny", "DA12 NNY")]
+    [InlineData("G Smith", "GSM 17H")]
+    [InlineData("James", "JAM 3S")]
+    public void GivenAPlate_WhenSearchingForRegistration_ThenReturnsTrueIfRegistrationIsSimilar(
+        string searchTerm,
+        string registration)
+    {
+        // Arrange
+        var searcher = new PlateFuzzySearcher();
+
+        // Act
+        var result = searcher.IsMatch(registration, searchTerm);
+
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void GivenAPlate_WhenSearchingForRegistration_ThenReturnsFalseIfRegistrationIsNotSimilar()
+    {
+        // Arrange
+        const string registration = "YT10 KUC";
+        const string searchTerm = "Dave";
+        
+        var searcher = new PlateFuzzySearcher();
+
+        // Act
+        var result = searcher.IsMatch(registration, searchTerm);
+
+        // Assert
+        Assert.False(result);
+    }
+}
